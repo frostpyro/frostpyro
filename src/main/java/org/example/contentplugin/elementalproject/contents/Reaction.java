817b2f3f61d5b42e4c812c2fc2000ddf1e523000
frostpyro <143861009+frostpyro2@users.noreply.github.com>
2023-09-03 22:27:26 +0900

    committing

--- src/main/java/org/example/contentplugin/elementalproject/contents/Reaction.java
+++ src/main/java/org/example/contentplugin/elementalproject/contents/Reaction.java
@@ -1,14 +1,107 @@
 package org.example.contentplugin.elementalproject.contents;
 
-import javax.swing.text.html.parser.Entity;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
 public class Reaction {
     private Map<UUID, Elements> react = new HashMap<>();
 
-    public void reaction(Entity entity, Elements elements){
+    public void hit(Player p, Entity entity, Elements elements){
+        if(!react.isEmpty()) return;
+        react.put(entity.getUniqueId(), elements);
+    }
 
+    public void reaction(Player p, Entity entity, Elements baseElement){
+        if(react.get(entity.getUniqueId())==null) return;
+
+        Elements elements = react.get(entity.getUniqueId());
+        if(baseElement == elements) return;
+        switch (elements){
+            case FIRE -> {
+                switch(baseElement){
+                    case AIR -> {
+
+                    }
+                    case ICE -> {
+
+                    }
+                    case EARTH -> {
+
+                    }
+                    case LIGHT -> {
+
+                    }
+                    case ELECTRONIC -> {
+
+                    }
+                }
+            }
+            case ICE -> {
+                switch(baseElement){
+                    case AIR -> {
+
+                    }
+                    case FIRE -> {
+
+                    }
+                    case EARTH -> {
+
+                    }
+                    case LIGHT -> {
+
+                    }
+                    case ELECTRONIC -> {
+
+                    }
+                }
+            }
+            case AIR -> {
+                List<Entity> entities = entity.getNearbyEntities(5,5,5);
+                for(Entity entity1 : entities){
+                    if(!(entity1 instanceof LivingEntity)) continue;
+                    if(entity1.equals(p))continue;
+                    if(react.containsKey(entity1.getUniqueId())){
+                        Elements newElement = react.get(entity1.getUniqueId());
+
+                        switch(newElement){
+                            case ICE->{
+
+                            }
+                            case EARTH -> {
+
+                            }
+                            case ELECTRONIC -> {
+
+                            }
+                            case FIRE -> {
+
+                            }
+                            case LIGHT -> {
+
+                            }
+                        }
+                    }
+                    else{
+                        react.put(entity1.getUniqueId(), elements);
+                        ((LivingEntity)entity1).damage(5, p);
+                    }
+                }
+            }
+            case EARTH -> {
+
+            }
+            case LIGHT -> {
+
+            }
+            case ELECTRONIC -> {
+
+            }
+        }
     }
 }
