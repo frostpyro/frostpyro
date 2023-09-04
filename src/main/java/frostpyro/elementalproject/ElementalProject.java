8f4f07ae5053029e54bf6dc4dfafc36d5a4c96a0
frostpyro <park35121454@gmail.com>
2023-09-04 12:29:18 +0900

    committing

---
+++ src/main/java/frostpyro/elementalproject/ElementalProject.java
@@ -0,0 +1,17 @@
+package frostpyro.elementalproject;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ElementalProject extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
