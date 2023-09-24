826efa1278bb8266908b57900556df1a8c161d71
frostpyro <143861009+frostpyro2@users.noreply.github.com>
2023-09-24 23:11:06 +0900

    committing

--- src/main/java/org/example/contentplugin/elementalproject/control/api/events/AttackEvent.java
+++ src/main/java/org/example/contentplugin/elementalproject/control/api/events/AttackEvent.java
@@ -1,34 +1,61 @@
 package org.example.contentplugin.elementalproject.control.api.events;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import javax.annotation.Nonnull;
 
 public class AttackEvent extends Event {
-    private final static HandlerList handler = new HandlerList();
+    private static final HandlerList handler = new HandlerList();
 
     private final PlayerInteractEvent event;
     private final EntityDamageEvent damageEvent;
-    public AttackEvent(PlayerInteractEvent event, EntityDamageEvent damageEvent) {
+
+    private final EntityDeathEvent deathEvent;
+    public AttackEvent(PlayerInteractEvent event, EntityDamageEvent damageEvent, EntityDeathEvent deathEvent) {
         super();
         this.event = event;
         this.damageEvent = damageEvent;
+        this.deathEvent = deathEvent;
     }
-
     public Player getPlayer(){
         return event.getPlayer();
     }
-
     public Entity getEntity(){
         return damageEvent.getEntity();
     }
+    public Player getKiller(){
+        return deathEvent.getEntity().getKiller();
+    }
 
-
-    @Override
+    public Material getMaterial(){
+        return event.getMaterial();
+    }
+    public ItemStack getItemMeta(){
+        return event.getItem();
+    }
+    public Entity getDeadEntity(){
+        return deathEvent.getEntity();
+    }
+    public Action getAction(){
+        return event.getAction();
+    }
+    @Nonnull
     public HandlerList getHandlers() {
+        return handler;
+    }
+
+    public static HandlerList getHandlerList(){
         return handler;
     }
 }
