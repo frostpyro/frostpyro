d89a78db1586d993121bf67a9362876dce901037
frostpyro <park35121454@gmail.com>
2023-11-01 14:16:15 +0900

    committing

--- src/main/java/org/example/contentplugin/elementalproject/interaction/click/Clicking.java
+++ src/main/java/org/example/contentplugin/elementalproject/interaction/click/Clicking.java
@@ -4,12 +4,18 @@ import org.bukkit.entity.Entity;
 import org.bukkit.entity.Interaction;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.example.contentplugin.elementalproject.interaction.Sequence;
 
-public class Clicking extends Sequence {
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
 
+public class Clicking extends Sequence {
+    private Set<UUID> damagedEntity = new HashSet<>();
     public void leftClick(EntityDamageByEntityEvent event){
         Entity entity = event.getEntity();
         if(!(entity instanceof Interaction)) return;
@@ -36,6 +42,45 @@ public class Clicking extends Sequence {
         resetArray(checkOut(array[0]), p);
     }
 
+    public void clickEntityLeft(EntityDamageByEntityEvent event){
+        Entity entity = event.getEntity();
+        if(!(event.getDamager() instanceof Player)) return;
+
+
+        if(!damagedEntity.contains(entity.getUniqueId())){
+            //TODO:write something
+            damagedEntity.add(entity.getUniqueId());
+        }
+        else{
+            event.setCancelled(true);
+            damagedEntity.remove(entity.getUniqueId());
+            return;
+        }
+    }
+
+    public void clickEntityRight(PlayerInteractAtEntityEvent event){
+        Entity entity = event.getRightClicked();
+        Player p = event.getPlayer();
+        if(!(entity instanceof LivingEntity)) return;
+    }
+
+    public void clickAirLeft(final PlayerInteractEvent event){
+        Player p = event.getPlayer();
+        Action a = event.getAction();
+        if(a != Action.LEFT_CLICK_AIR && a != Action.LEFT_CLICK_BLOCK) return;
+
+
+        event.setCancelled(true);
+    }
+
+    public void clickAirRight(final PlayerInteractEvent event){
+        Player p = event.getPlayer();
+        Action a = event.getAction();
+        if(a != Action.RIGHT_CLICK_AIR && a != Action.RIGHT_CLICK_BLOCK) return;
+
+
+        event.setCancelled(true);
+    }
 
     private boolean checkOut(int data){
         return data != 0;
