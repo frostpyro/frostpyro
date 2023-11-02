12d33641fca3d4c8580a3a0964e6e74dbfa78755
frostpyro <park35121454@gmail.com>
2023-11-02 16:13:12 +0900

    committing

--- src/main/java/org/example/contentplugin/elementalproject/contents/playerSkill/attackMethod/RLL.java
+++ src/main/java/org/example/contentplugin/elementalproject/contents/playerSkill/attackMethod/RLL.java
@@ -1,7 +1,11 @@
 package org.example.contentplugin.elementalproject.contents.playerSkill.attackMethod;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerInteractEvent;
 
+import java.util.Set;
+import java.util.UUID;
+
 public interface RLL {
-    void attacking(PlayerInteractEvent event);
+    void attacking(Player player, Set<UUID> entitySet);
 }
