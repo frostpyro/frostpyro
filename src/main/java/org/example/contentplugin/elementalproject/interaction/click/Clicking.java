349c859749033bb17fa243d0600bffcf5420f32f
frostpyro <park35121454@gmail.com>
2023-11-03 11:27:27 +0900

    committing

--- src/main/java/org/example/contentplugin/elementalproject/interaction/click/Clicking.java
+++ src/main/java/org/example/contentplugin/elementalproject/interaction/click/Clicking.java
@@ -1,5 +1,6 @@
 package org.example.contentplugin.elementalproject.interaction.click;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Interaction;
 import org.bukkit.entity.LivingEntity;
@@ -9,10 +10,13 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.example.contentplugin.elementalproject.contents.playerSkill.Damaging;
 import org.example.contentplugin.elementalproject.interaction.Sequence;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 import java.util.UUID;
 
@@ -53,6 +57,7 @@ public class Clicking extends Sequence {
 
         if (!(event.getDamager() instanceof Player)) return;
         Player p = (Player) event.getDamager();
+        if(!itemCheck(p)) return;
         if(!damagedEntity.contains(entity.getUniqueId())) {
             damaging.damageMethod(p, damagedEntity);
             // TODO: write something
@@ -65,27 +70,43 @@ public class Clicking extends Sequence {
         Entity entity = event.getRightClicked();
         Player p = event.getPlayer();
         if(!(entity instanceof LivingEntity)) return;
+        if(!itemCheck(p)) return;
     }
 
     public void clickAirLeft(final PlayerInteractEvent event){
         Player p = event.getPlayer();
         Action a = event.getAction();
+        if(!itemCheck(p)) return;
         if(a != Action.LEFT_CLICK_AIR && a != Action.LEFT_CLICK_BLOCK) return;
 
         damaging.damageMethod(p, damagedEntity);
+        event.setCancelled(true);
     }
 
     public void clickAirRight(final PlayerInteractEvent event){
         Player p = event.getPlayer();
         Action a = event.getAction();
+        if(!itemCheck(p)) return;
         if(a != Action.RIGHT_CLICK_AIR && a != Action.RIGHT_CLICK_BLOCK) return;
 
 
-
+        event.setCancelled(true);
     }
 
     private boolean checkOut(int data){
         return data != 0;
     }
-
+    @SuppressWarnings("all")
+    private boolean itemCheck(Player p){
+        ItemStack item = p.getInventory().getItemInMainHand();
+        if(!item.hasItemMeta()) return false;
+        if(!item.getItemMeta().hasCustomModelData()) return false;
+        Material material = item.getType();
+        switch(material){
+            case NETHERITE_SWORD, DIAMOND_SWORD, WOODEN_HOE, BOW -> {
+                return item.getItemMeta().getCustomModelData() == 1;
+            }
+        }
+        return false;
+    }
 }
