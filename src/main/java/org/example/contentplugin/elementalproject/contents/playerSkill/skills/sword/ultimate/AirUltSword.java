fe2e5b9b2ae50b85d2d0fe3db6e14a11dce5af4e
frostpyro <park35121454@gmail.com>
2023-11-16 15:43:45 +0900

    committing

--- src/main/java/org/example/contentplugin/elementalproject/contents/playerSkill/skills/sword/ultimate/AirUltSword.java
+++ src/main/java/org/example/contentplugin/elementalproject/contents/playerSkill/skills/sword/ultimate/AirUltSword.java
@@ -1,14 +1,31 @@
 package org.example.contentplugin.elementalproject.contents.playerSkill.skills.sword.ultimate;
 
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.example.contentplugin.elementalproject.ElementalProject;
 import org.example.contentplugin.elementalproject.contents.playerSkill.attackMethod.SNS;
+import org.example.contentplugin.elementalproject.contents.playerSkill.skills.BaseAttackEnhancer;
 
 import java.util.Set;
 import java.util.UUID;
 
 public class AirUltSword implements SNS {
+    private int sec;
+
+    AirUltSword(int sec){
+        this.sec = sec;
+    }
+    BaseAttackEnhancer enhancer = new BaseAttackEnhancer();
     @Override
     public void attacking(Player player, Set<UUID> entitySet) {
+        if(!enhancer.enhancer(player)) return;
+        enhancer.setEnhance(player, 1);
 
+        new BukkitRunnable(){
+            @Override
+            public void run() {
+                enhancer.setEnhance(player, 0);
+            }
+        }.runTaskLater(ElementalProject.getPlugin(), sec* 20L);
     }
 }
