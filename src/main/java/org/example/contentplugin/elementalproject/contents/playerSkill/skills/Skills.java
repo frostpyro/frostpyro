86fa2bacf17dea065b7129c139d6e14429aa0e08
frostpyro <143861009+frostpyro2@users.noreply.github.com>
2023-11-24 23:16:17 +0900

    committing

--- src/main/java/org/example/contentplugin/elementalproject/contents/playerSkill/skills/Skills.java
+++ src/main/java/org/example/contentplugin/elementalproject/contents/playerSkill/skills/Skills.java
@@ -4,11 +4,13 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
+import org.example.contentplugin.elementalproject.ElementalProject;
 import org.example.contentplugin.elementalproject.SQLDB.DataBase;
 import org.example.contentplugin.elementalproject.SQLDB.playerData.PlayerStat;
 import org.example.contentplugin.elementalproject.contents.playerSkill.attackMethod.*;
 import org.example.contentplugin.elementalproject.contents.playerSkill.skills.sword.baseAttack.*;
-import org.example.contentplugin.elementalproject.contents.playerSkill.skills.sword.enhance.AirEnhanceSword;
 import org.example.contentplugin.elementalproject.contents.playerSkill.skills.sword.skill1.*;
 import org.example.contentplugin.elementalproject.contents.playerSkill.skills.sword.skill2.*;
 import org.example.contentplugin.elementalproject.contents.playerSkill.skills.sword.skill3.*;
@@ -22,34 +24,11 @@ public class Skills {
 
     private final DataBase dataBase = new DataBase();
 
-    private final BaseAttack airBaseSword = new AirBaseSword();
-    private final BaseAttack electBaseSword = new ElectBaseSword();
-    private final BaseAttack fireBaseSword = new FireBaseSword();
-    private final BaseAttack iceBaseSword = new IceBaseSword();
-    private final BaseAttack lightBaseSword = new LightBaseSword();
-    private final BaseAttack earthBaseSword = new EarthBaseSword();
-
-    private final BaseAttack airEnhanceSword = new AirEnhanceSword();
-    private final Right airSkill1Sword = new AirSkill1Sword();
-    private final Right electSkill1Sword = new ElectSkill1Sword();
-    private final Right earthSkill1Sword = new EarthSkill1Sword();
-    private final Right iceSkill1Sword = new IceSkill1Sword();
-    private final Right fireSkill1Sword = new FireSkill1Sword();
-    private final Right lightSkill1Sword = new LightSkill1Sword();
-    private final RNS iceSkill2Sword = new IceSkill2Sword();
-    private final RNS fireSKill2Sword = new FireSkill2Sword();
-    private final RNS earthSkill2Sword = new EarthSkill2Sword();
-    private final RNS electSkill2Sword = new ElectSkill2Sword();
-    private final RNS lightSkill2Sword = new LightSkill2Sword();
-    private final RNS airSkill2Sword = new AirSkill2Sword();
-    private final LNS airSkill3Sword = new AirSkill3Sword();
-    private final LNS earthSkill3Sword = new EarthSkill3Sword();
-    private final LNS fireSkill3Sword = new FireSkill3Sword();
-    private final LNS electSkill3Sword = new ElectSkill3Sword();
-    private final LNS iceSkill3Sword = new IceSkill3Sword();
-    private final LNS lightSkill3Sword = new LightSkill3Sword();
-
-    private final SNS airUltSword = new AirUltSword(3);
+    private BaseAttack baseAttack;
+    private Right right;
+    private RNS rns;
+    private LNS lns;
+    private SNS sns;
 
     private PlayerStat getPlayerStat(Player p) throws SQLException {
         PlayerStat playerStat = dataBase.findUUID(p.getUniqueId().toString());
@@ -66,6 +45,10 @@ public class Skills {
     public void baseAttackSkill(Player p, Set<UUID> entitySet, double sec){
         nullCheck(p);
         if(p.isSneaking()) return;
+        PersistentDataContainer container = p.getPersistentDataContainer();
+        if(!container.has(ElementalProject.skill(), PersistentDataType.INTEGER_ARRAY)) container.set(ElementalProject.skill(), PersistentDataType.INTEGER_ARRAY, new int[5]);
+
+        int getSkill = container.get(ElementalProject.skill(), PersistentDataType.INTEGER_ARRAY)[0];
         try{
             PlayerStat playerStat = getPlayerStat(p);
             int skillClass = playerStat.getSkillClass();
@@ -77,12 +60,14 @@ public class Skills {
                 switch(skillClass){
                     case 1 ->{
                         if(!getItem(p, Material.NETHERITE_SWORD, 1)) return;
-                        airBaseSword.attacking(p, entitySet);
-                        earthBaseSword.attacking(p, entitySet);
-                        fireBaseSword.attacking(p, entitySet);
-                        iceBaseSword.attacking(p, entitySet);
-                        lightBaseSword.attacking(p, entitySet);
-                        electBaseSword.attacking(p, entitySet);
+                        switch(getSkill){
+                            case 1 -> baseAttack = new AirBaseSword();
+                            case 2 -> baseAttack = new EarthBaseSword();
+                            case 3 -> baseAttack = new ElectBaseSword();
+                            case 4 -> baseAttack = new FireBaseSword();
+                            case 5 -> baseAttack = new IceBaseSword();
+                            case 6 -> baseAttack = new LightBaseSword();
+                        }
                     }
                     case 2 ->{
                         if(!getItem(p, Material.WOODEN_SWORD, 1)) return;
@@ -99,7 +84,7 @@ public class Skills {
                 switch (skillClass){
                     case 1 ->{
                         if(!getItem(p, Material.NETHERITE_SWORD, 1)) return;
-                        airEnhanceSword.attacking(p, entitySet);
+
                     }
                     case 2 ->{
                         if(!getItem(p, Material.WOODEN_SWORD, 1)) return;
@@ -112,6 +97,7 @@ public class Skills {
                     }
                 }
             }
+            baseAttack.attacking(p, entitySet);
             cooldowns.get(p.getUniqueId())[0] = System.currentTimeMillis() + (long)(sec*1000L);
         }
         catch (SQLException e){
@@ -122,6 +108,10 @@ public class Skills {
     public void skill1(Player p, Set<UUID> entitySet, long sec){
         nullCheck(p);
         if(p.isSneaking()) return;
+        PersistentDataContainer container = p.getPersistentDataContainer();
+        if(!container.has(ElementalProject.skill(), PersistentDataType.INTEGER_ARRAY)) container.set(ElementalProject.skill(), PersistentDataType.INTEGER_ARRAY, new int[5]);
+
+        int getSkill = container.get(ElementalProject.skill(), PersistentDataType.INTEGER_ARRAY)[1];
         try{
             PlayerStat playerStat = getPlayerStat(p);
             int skillClass = playerStat.getSkillClass();
@@ -132,12 +122,7 @@ public class Skills {
             switch(skillClass){
                 case 1 ->{
                     if(!getItem(p, Material.NETHERITE_SWORD, 1)) return;
-                    airSkill1Sword.attacking(p, entitySet);
-                    fireSkill1Sword.attacking(p, entitySet);
-                    iceSkill1Sword.attacking(p, entitySet);
-                    earthSkill1Sword.attacking(p, entitySet);
-                    lightSkill1Sword.attacking(p, entitySet);
-                    electSkill1Sword.attacking(p, entitySet);
+                    right = new AirSkill1Sword();
                 }
                 case 2 ->{
                     if(!getItem(p, Material.WOODEN_SWORD, 1)) return;
@@ -149,6 +134,7 @@ public class Skills {
                     if(!getItem(p, Material.WOODEN_HOE, 1)) return;
                 }
             }
+            right.attacking(p, entitySet);
             cooldowns.get(p.getUniqueId())[1] = System.currentTimeMillis() + (sec * 1000);
         }
         catch (SQLException e){
@@ -159,6 +145,10 @@ public class Skills {
     public void skill2(Player p, Set<UUID> entitySet, long sec){
         if(!p.isSneaking()) return;
         nullCheck(p);
+        PersistentDataContainer container = p.getPersistentDataContainer();
+        if(!container.has(ElementalProject.skill(), PersistentDataType.INTEGER_ARRAY)) container.set(ElementalProject.skill(), PersistentDataType.INTEGER_ARRAY, new int[5]);
+
+        int getSkill = container.get(ElementalProject.skill(), PersistentDataType.INTEGER_ARRAY)[2];
         try{
             PlayerStat playerStat = getPlayerStat(p);
             int skillClass = playerStat.getSkillClass();
@@ -169,12 +159,7 @@ public class Skills {
             switch(skillClass){
                 case 1 ->{
                     if(!getItem(p, Material.NETHERITE_SWORD, 1)) return;
-                    airSkill2Sword.attacking(p, entitySet);
-                    electSkill2Sword.attacking(p, entitySet);
-                    iceSkill2Sword.attacking(p, entitySet);
-                    fireSKill2Sword.attacking(p, entitySet);
-                    earthSkill2Sword.attacking(p, entitySet);
-                    lightSkill2Sword.attacking(p, entitySet);
+                    rns = new AirSkill2Sword();
                 }
                 case 2 ->{
                     if(!getItem(p, Material.WOODEN_SWORD, 1)) return;
@@ -186,6 +171,7 @@ public class Skills {
                     if(!getItem(p, Material.WOODEN_HOE, 1)) return;
                 }
             }
+            rns.attacking(p, entitySet);
             cooldowns.get(p.getUniqueId())[2] = System.currentTimeMillis() + (sec * 1000);
         }
         catch (SQLException e){
@@ -196,6 +182,10 @@ public class Skills {
     public void skill3(Player p, Set<UUID> entitySet, long sec){
         if(!p.isSneaking()) return;
         nullCheck(p);
+        PersistentDataContainer container = p.getPersistentDataContainer();
+        if(!container.has(ElementalProject.skill(), PersistentDataType.INTEGER_ARRAY)) container.set(ElementalProject.skill(), PersistentDataType.INTEGER_ARRAY, new int[5]);
+
+        int getSkill = container.get(ElementalProject.skill(), PersistentDataType.INTEGER_ARRAY)[3];
         try{
             PlayerStat playerStat = getPlayerStat(p);
             if(cooldowns.get(p.getUniqueId())[3] != 0){
@@ -206,7 +196,7 @@ public class Skills {
             switch(skillClass){
                 case 1 ->{
                     if(!getItem(p, Material.NETHERITE_SWORD, 1)) return;
-                    airSkill3Sword.attacking(p, entitySet);
+                    lns = new AirSkill3Sword();
                 }
                 case 2 ->{
                     if(!getItem(p, Material.WOODEN_SWORD, 1)) return;
@@ -218,15 +208,20 @@ public class Skills {
                     if(!getItem(p, Material.WOODEN_HOE, 1)) return;
                 }
             }
+            lns.attacking(p, entitySet);
+            cooldowns.get(p.getUniqueId())[3] = System.currentTimeMillis() + (sec * 1000);
         }
         catch (SQLException e){
             e.printStackTrace();
         }
-        cooldowns.get(p.getUniqueId())[3] = System.currentTimeMillis() + (sec * 1000);
     }
 
     public void ultimateSkill(Player p, Set<UUID> entitySet, long sec){
         nullCheck(p);
+        PersistentDataContainer container = p.getPersistentDataContainer();
+        if(!container.has(ElementalProject.skill(), PersistentDataType.INTEGER_ARRAY)) container.set(ElementalProject.skill(), PersistentDataType.INTEGER_ARRAY, new int[5]);
+
+        int getSkill = container.get(ElementalProject.skill(), PersistentDataType.INTEGER_ARRAY)[4];
         try{
             PlayerStat playerStat = getPlayerStat(p);
             int skillClass = playerStat.getSkillClass();
@@ -237,7 +232,7 @@ public class Skills {
             switch(skillClass){
                 case 1 ->{
                     if(!getItem(p, Material.NETHERITE_SWORD, 1)) return;
-                    airUltSword.attacking(p, entitySet);
+                    sns = new AirUltSword(3);
                 }
                 case 2 ->{
                     if(!getItem(p, Material.WOODEN_SWORD, 1)) return;
@@ -249,6 +244,7 @@ public class Skills {
                     if(!getItem(p, Material.WOODEN_HOE, 1)) return;
                 }
             }
+            sns.attacking(p, entitySet);
             cooldowns.get(p.getUniqueId())[4] = System.currentTimeMillis() + (sec * 1000);
         }
         catch (SQLException e){
