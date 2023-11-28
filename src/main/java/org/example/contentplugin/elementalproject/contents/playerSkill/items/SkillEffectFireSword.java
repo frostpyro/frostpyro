1d09ab8806cea71a7df6cf9c8c69911c33706fd8
frostpyro <143861009+frostpyro2@users.noreply.github.com>
2023-11-28 23:05:33 +0900

    committing

--- src/main/java/org/example/contentplugin/elementalproject/contents/playerSkill/items/SkillEffectFireSword.java
+++ src/main/java/org/example/contentplugin/elementalproject/contents/playerSkill/items/SkillEffectFireSword.java
@@ -94,6 +94,32 @@ public class SkillEffectFireSword {
         Random random = new Random();
         ItemDisplay display = (ItemDisplay) p.getWorld().spawnEntity(p.getLocation().add(p.getLocation().getDirection().multiply(2)).add(0, 1.2,0), EntityType.ITEM_DISPLAY);
 
+        int c = random.nextInt(3);
+
+        Transformation transformation = display.getTransformation();
+        transformation.getScale().set(12f);
+        Location location = p.getLocation();
+
+        switch(c){
+            case 1 ->transformation.getRightRotation().z = 0.5f;
+            case 2 ->transformation.getLeftRotation().z = -1;
+        }
+
+        World w = p.getWorld();
+
+        display.setTransformation(transformation);
+        display.setBrightness(new Display.Brightness(15, 15));
+        w.playSound(location, Sound.ENTITY_BLAZE_SHOOT, 2, 1);
+        for(Entity entity : display.getNearbyEntities(10,10,10)){
+            if(entity == p) continue;
+            if(!(entity instanceof LivingEntity)) continue;
+
+            if((entity.getLocation().distance(display.getLocation()) > 4.5)) continue;
+
+            entitySet.add(entity.getUniqueId());
+            ((LivingEntity)entity).damage(5, p);
+        }
+
         for(int i = 1; i <= 10; i++){
             int temp = i;
             new BukkitRunnable(){
@@ -109,22 +135,6 @@ public class SkillEffectFireSword {
         }
 
 
-        Transformation transformation = display.getTransformation();
-        transformation.getScale().set(12f);
-        Location location = p.getLocation();
-        World w = p.getWorld();
-        display.setTransformation(transformation);
-        display.setBrightness(new Display.Brightness(15, 15));
-        w.playSound(location, Sound.ENTITY_BLAZE_SHOOT, 2, 1);
-        for(Entity entity : display.getNearbyEntities(10,10,10)){
-            if(entity == p) continue;
-            if(!(entity instanceof LivingEntity)) continue;
-
-            if((entity.getLocation().distance(display.getLocation()) > 4.5)) continue;
-
-            entitySet.add(entity.getUniqueId());
-            ((LivingEntity)entity).damage(5, p);
-        }
 
         new BukkitRunnable(){
             @Override
