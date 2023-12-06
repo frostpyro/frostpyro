5eae86125f0d2534909639790cc2c990f5b14dca
frostpyro <park35121454@gmail.com>
2023-12-06 16:22:39 +0900

    committing

--- src/main/java/org/example/contentplugin/elementalproject/system/skillSetting/SkillList.java
+++ src/main/java/org/example/contentplugin/elementalproject/system/skillSetting/SkillList.java
@@ -7,11 +7,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SkillList {
-    private List<BaseAttackEnum> baseEnum = new ArrayList<>();
-    private List<Skill1Enum> skill1Enum = new ArrayList<>();
-    private List<Skill2Enum> skill2Enum = new ArrayList<>();
-    private List<Skill3Enum> skill3Enum = new ArrayList<>();
-    private List<UltEnum> ultEnum = new ArrayList<>();
+    private final List<BaseAttackEnum> baseEnum = new ArrayList<>();
+    private final List<Skill1Enum> skill1Enum = new ArrayList<>();
+    private final List<Skill2Enum> skill2Enum = new ArrayList<>();
+    private final List<Skill3Enum> skill3Enum = new ArrayList<>();
+    private final List<UltEnum> ultEnum = new ArrayList<>();
 
 
 }
