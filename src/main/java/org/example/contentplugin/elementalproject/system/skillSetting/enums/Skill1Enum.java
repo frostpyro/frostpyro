0713d0fe4c144ec0beca8004b053870f295c5fe3
frostpyro <143861009+frostpyro2@users.noreply.github.com>
2023-12-10 00:52:04 +0900

    committing

--- src/main/java/org/example/contentplugin/elementalproject/system/skillSetting/enums/Skill1Enum.java
+++ src/main/java/org/example/contentplugin/elementalproject/system/skillSetting/enums/Skill1Enum.java
@@ -1,10 +1,15 @@
 package org.example.contentplugin.elementalproject.system.skillSetting.enums;
 
 import org.example.contentplugin.elementalproject.contents.playerSkill.attackMethod.Right;
-import org.example.contentplugin.elementalproject.contents.playerSkill.skills.sword.skill1.AirSkill1Sword;
+import org.example.contentplugin.elementalproject.contents.playerSkill.skills.sword.skill1.*;
 
 public enum Skill1Enum {
-    AS(AirSkill1Sword.class);
+    AS(AirSkill1Sword.class),
+    ES(EarthSkill1Sword.class),
+    ElS(ElectSkill1Sword.class),
+    FS(FireSkill1Sword.class),
+    IS(IceSkill1Sword.class),
+    LS(LightSkill1Sword.class)   ;
 
     private final Class<? extends Right> skill1;
 
